e632dd6844d0efa4e4bc09a2d7aa41cf9bfbda48
MrShadowRIFAT <amannan.rifat.bd@gmail.com>
2026-03-25 22:52:15 +0600

    Initial commit from GitSync

---
+++ components/About/AboutData.jsx
@@ -0,0 +1,35 @@
+import HeroAvatar from '@/public/images/hero-avatar.jpg';
+
+export const aboutData = {
+    mainData: {
+        title: "About Me",
+        title2: "About Me",
+        heroAvatar: HeroAvatar,
+        jobTitle: "UI & UX Designer. Photographer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        yearsOfExperience: "12",
+        hoursOfWorking: "20",
+        projectsDone: "90",
+        typewriter: ['Hi There!', 'I\'m Christina'],
+    },
+    skills: [
+        {
+            id: 1,
+            name: 'HTML5 & CSS3',
+            percent: '94',
+            fontawesomeIcon: 'fa-brands fa-html5',
+        },
+        {
+            id: 2,
+            name: 'Bootstrap',
+            percent: '98',
+            fontawesomeIcon: 'fa-brands fa-bootstrap',
+        },
+        {
+            id: 3,
+            name: 'SEO',
+            percent: '75',
+            fontawesomeIcon: 'fa-solid fa-magnifying-glass',
+        },
+    ]
+};
---
+++ components/Header/HeaderData.jsx
@@ -0,0 +1,40 @@
+export const headerData = {
+    mainData: {
+        firstName: 'Christina',
+        secondName: 'Gray',
+        phone: '+(976) 12 34 9999',
+        email: 'flatheme@gmail.com',
+        copywriter: 'FlaTheme',
+        facebookURL: '#',
+        twitterURL: '#',
+        youtubeURL: '#',
+    },
+    links: [
+        {
+            name: 'Link Item 1',
+            url: '#'
+        },
+        {
+            name: 'Link Item 2',
+            url: '#'
+        },
+    ],
+    socialLinks: [
+        {
+            name: 'FB',
+            url: '#'
+        },
+        {
+            name: 'TW',
+            url: '#'
+        },
+        {
+            name: 'IG',
+            url: '#'
+        },
+        {
+            name: 'IN',
+            url: '#'
+        },
+    ]
+};
